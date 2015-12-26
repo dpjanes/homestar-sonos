0e7f0ed17fa84b1c147a06e2be46c7dc79edb38e
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:48 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -36,10 +36,9 @@ echo "=================="
         SonosBridge.js index.js \
         models/*js \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
