97cd4e591c6ceb4acef2b849075f33968e6fb11b
David Janes <davidjanes@davidjanes.com>
2016-11-03 10:14:29 -0400

    fix package 'homepage'

--- SonosBridge.js
+++ SonosBridge.js
@@ -63,10 +63,6 @@ var SonosBridge = function (initd, native) {
 
 SonosBridge.prototype = new iotdb.Bridge();
 
-SonosBridge.prototype.name = function () {
-    return "SonosBridge";
-};
-
 /* --- lifecycle --- */
 
 /**
